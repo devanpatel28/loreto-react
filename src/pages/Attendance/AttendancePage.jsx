07ddaf0e87db238f0a26bbb2e4a8f3812f7973fe
Devan Bhensdadiya <92723752+devanpatel28@users.noreply.github.com>
2024-05-04 21:48:18 +0530

    Project Done

--- src/pages/Attendance/AttendancePage.jsx
+++ src/pages/Attendance/AttendancePage.jsx
@@ -15,6 +15,7 @@ const AttendancePage = () => {
     const navigate = useNavigate();
     const [search, setSearch] = useState('');
     const [Data, setData] = useState([]);
+    const [oldData, setOldData] = useState([]);
     const [selectedDate, setSelectedDate] = useState(new Date()); // State to manage selected date
     const [shifts, setShifts] = useState([]);
     const [selectedShift, setSelectedShift] = useState('');
@@ -36,7 +37,7 @@ const AttendancePage = () => {
     const addTodayStudent = async () => {
         try {
             const response = await axios.post(ATTENDANCE_API, { date: format(new Date(), "yyyy-MM-dd") });
-            getTodayStudent();
+            // getTodayStudent();
         } catch (error) {
             console.error("Error fetching data:", error);
 
@@ -47,11 +48,29 @@ const AttendancePage = () => {
         try {
             const response = await axios.get(ATTENDANCE_API);
             setData(response.data.data);
+            setOldData(response.data.data);
         } catch (error) {
             console.error("Error fetching data:", error);
         }
-    };
 
+    };
+    const saveAttendance = async () => {
+        try {
+            await axios.put(UPDATE_ATTENDANCE_API, Data);
+            getTodayStudent();
+            setData([]);
+            setOldData([]);
+            Swal.fire({
+                icon: "success",
+                text: "Attendance Updated",
+                showConfirmButton: false,
+                width: "20rem",
+                timer: 1000,
+            });
+        } catch (error) {
+            console.error("Error updating attendance:", error);
+        }
+    }
 
     const handleDateChange = (date) => {
         setSelectedDate(date);
@@ -68,14 +87,6 @@ const AttendancePage = () => {
             (selectedShift === '' || attendance.shiftdatum_id == selectedShift) // Check for selected shift
         );
     });
-    const handleStatusChange = async (id, status) => {
-        try {
-            await axios.put(UPDATE_ATTENDANCE_API, [{ id, status }]);
-            getTodayStudent();
-        } catch (error) {
-            console.error("Error updating status:", error);
-        }
-    }
 
     return (
         <DefaultLayout>
@@ -85,7 +96,7 @@ const AttendancePage = () => {
                     <input
                         type="text"
                         placeholder="Search Student"
-                        className="border w-100 rounded-sm focus:outline-none border-stroke bg-white py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                        className="border w-90 rounded-sm focus:outline-none border-stroke bg-white py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                         value={search}
                         onChange={(e) => setSearch(e.target.value)}
                     />
@@ -108,7 +119,15 @@ const AttendancePage = () => {
                             </option>
                         ))}
                     </select>
+            
                 </form>
+                <div className="ml-19 font-medium text-black dark:text-white">
+                {oldData.length !== Data.length || oldData.some((item, index) => item.id !== Data[index].id || item.status !== Data[index].status) ? (
+                    <button onClick={saveAttendance} className="w-40 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium shadow-sm text-white bg-graydark hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary">
+                        Save Attendance
+                    </button>
+                ) : null}
+            </div>
             </div>
 
             <div className="rounded-sm border border-stroke bg-white px-3 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-5 xl:pb-1">
@@ -126,7 +145,9 @@ const AttendancePage = () => {
                                 <th className="py-4 px-6 font-medium text-black dark:text-white">
                                     Status
                                 </th>
+
                             </tr>
+
                         </thead>
                         <tbody>
                             {filteredData.map((attendance, key) => (
@@ -145,17 +166,23 @@ const AttendancePage = () => {
                                     <td className="border-b border-[#eee] py-2 px-4 dark:border-strokedark">
                                         <div className="flex gap-2">
                                             <button
-                                                onClick={() => handleStatusChange(attendance.id, "Present")}
+                                                onClick={() => setData(prevData => (
+                                                    prevData.map(item => item.id === attendance.id ? { ...item, status: "Present" } : item)
+                                                ))}
                                                 className={`inline-flex rounded-full bg-opacity-10 py-1 px-3 text-sm font-medium ${attendance.status === "Present" ? 'bg-success text-success' : 'bg-gray-400 text-gray-200'}`}>
                                                 Present
                                             </button>
                                             <button
-                                                onClick={() => handleStatusChange(attendance.id, "Absent")}
+                                                onClick={() => setData(prevData => (
+                                                    prevData.map(item => item.id === attendance.id ? { ...item, status: "Absent" } : item)
+                                                ))}
                                                 className={`inline-flex rounded-full bg-opacity-10 py-1 px-3 text-sm font-medium ${attendance.status === "Absent" ? 'bg-danger text-danger' : 'bg-gray-400 text-gray-200'}`}>
                                                 Absent
                                             </button>
                                             <button
-                                                onClick={() => handleStatusChange(attendance.id, "On Leave")}
+                                                onClick={() => setData(prevData => (
+                                                    prevData.map(item => item.id === attendance.id ? { ...item, status: "On Leave" } : item)
+                                                ))}
                                                 className={`inline-flex rounded-full bg-opacity-10 py-1 px-3 text-sm font-medium ${attendance.status === "On Leave" ? 'bg-warning text-warning' : 'bg-gray-400 text-gray-200'}`}>
                                                 On Leave
                                             </button>
@@ -163,6 +190,7 @@ const AttendancePage = () => {
                                     </td>
                                 </tr>
                             ))}
+
                         </tbody>
 
                     </table>
